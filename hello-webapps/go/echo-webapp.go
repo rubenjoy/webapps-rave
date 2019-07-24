79cddf9422fc517d1440b87e379acf421bdd7fcc
ruben juliarto <v-ruben.juliarto@btpn.com>
2019-07-24 18:36:55 +0700

    PerfTest Tools Dockerfile
     - add perftest tools dockerfile
     - add flag argument to echo webapp
     - add remove apk cache go dockerfile

--- hello-webapps/go/echo-webapp.go
+++ hello-webapps/go/echo-webapp.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 	"github.com/labstack/echo"
 )
 
+var addr = flag.String("addr", ":3000", "http service address")
+
 func main() {
+	flag.Parse()
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":3000"))
+	e.Logger.Fatal(e.Start(*addr))
 }
